f47c4c3cc3ae109efc4d0df04784844f6cc709b4
Rarder44 <lpozzi44@gmail.com>
2015-04-17 00:31:30 +0200

    Aggiunta Launcher

---
+++ ESOLanguageLauncher/ESOLanguageLauncher/Save.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+namespace ESOLanguageLauncher
+{
+    public class Save
+    {
+
+        public String PathUserSettings = "";
+        public String PathEso_exe = "";
+        public String lang = "";
+
+        [XmlIgnore]
+        String ConfFileName = "conf.xml";
+
+        
+        public Save()
+        {
+        }
+
+        public bool IsComplete()
+        {
+            return PathUserSettings.Trim() != "" && File.Exists(PathUserSettings) && PathEso_exe.Trim() != "" && File.Exists(PathEso_exe) && lang.Trim() != "";
+        }
+
+        public bool LoadFromFile()
+        {
+            if (File.Exists(ConfFileName))
+            {
+                XmlSerializer s = new XmlSerializer(this.GetType());
+                StreamReader r = new StreamReader(ConfFileName);
+                this.SetSave((Save)s.Deserialize(r));
+                r.Close();
+                return true;
+            }
+            else 
+                return false;
+        }
+        public void WriteToFile()
+        {
+           XmlSerializer s = new XmlSerializer(this.GetType());
+           StreamWriter w = new StreamWriter(ConfFileName);
+           s.Serialize(w, this);
+           w.Close();
+        }
+
+        public void SetSave(Save o)
+        {
+            this.PathUserSettings = o.PathUserSettings;
+            this.PathEso_exe = o.PathEso_exe;
+            this.lang = o.lang;
+        }
+
+      
+    }
+}
---
+++ ESOLanguageLauncher/README.md
@@ -0,0 +1,10 @@
+# ESO LL
+Prima bozza di un "Launcher"
+
+
+Spiegazioni varie:
+Coming Soon...
+
+
+
+Compilare con Visual Studio Community (free)
